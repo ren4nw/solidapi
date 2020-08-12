d567e0960b7c6919ce8fde834eab6bcf1e0e4784
ren4nw <yung66@protonmail.com>
2020-08-12 18:49:10 -0300

    Create route for create user

--- src/useCases/CreateUser/index.ts
+++ src/useCases/CreateUser/index.ts
@@ -1,6 +1,7 @@
 import { MailtrapMailProvider } from "../../providers/implementatios/MailtrapMailProvider";
 import { PostgresUsersRepository } from "../../repositories/implementatios/PostgresUsersRepository";
 import { CreateUserUseCase } from "./CreateUserUseCase";
+import { CreateUserController } from "./CreateUserController";
 
 const mailtrapMailProvider = new MailtrapMailProvider();
 const postgresUsersRepository = new PostgresUsersRepository();
@@ -9,3 +10,9 @@ const createUserUseCase = new CreateUserUseCase(
     postgresUsersRepository,
     mailtrapMailProvider
 );
+
+const createUserController = new CreateUserController(
+    createUserUseCase
+);
+
+export { createUserUseCase, createUserController };
