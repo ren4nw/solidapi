f83cafa9140ddf43270072b625a32c3877e0cfed
ren4nw <yung66@protonmail.com>
2020-08-12 18:45:54 -0300

    Create providers and implement them

--- src/useCases/CreateUser/CreateUserUseCase.ts
+++ src/useCases/CreateUser/CreateUserUseCase.ts
@@ -1,10 +1,12 @@
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 import { ICreateUserRequestDTO } from "./CreateUserDTO";
 import { User } from "../../entities/User";
+import { IMailProvider } from "../../providers/IMailProvider";
 
 export class CreateUserUseCase {
     constructor(
-        private usersRepository: IUsersRepository
+        private usersRepository: IUsersRepository,
+        private mailProvider: IMailProvider
     ) {}
 
     async execute(data: ICreateUserRequestDTO) {
@@ -17,5 +19,18 @@ export class CreateUserUseCase {
         const user = new User(data);
 
         await this.usersRepository.save(user);
+
+        this.mailProvider.sendMail({
+            to: {
+                name: data.name,
+                email: data.email
+            },
+            from: {
+                name: 'Equipe do Meu App',
+                email: 'equipe@meuapp.com'
+            },
+            subject: 'Seja bem-vindo á plataforma',
+            body: '<p>Você já pode fazer login em nossa plataforma.</p>'
+        });
     }
 }
---
+++ src/useCases/CreateUser/index.ts
@@ -0,0 +1,11 @@
+import { MailtrapMailProvider } from "../../providers/implementatios/MailtrapMailProvider";
+import { PostgresUsersRepository } from "../../repositories/implementatios/PostgresUsersRepository";
+import { CreateUserUseCase } from "./CreateUserUseCase";
+
+const mailtrapMailProvider = new MailtrapMailProvider();
+const postgresUsersRepository = new PostgresUsersRepository();
+
+const createUserUseCase = new CreateUserUseCase(
+    postgresUsersRepository,
+    mailtrapMailProvider
+);
